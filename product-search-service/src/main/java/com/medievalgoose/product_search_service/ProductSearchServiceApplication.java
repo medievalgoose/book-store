a5aab75d46765358d279be230e741f5bc15f568a
medievalgoose <medievalgoose@gmail.com>
2024-07-08 08:01:03 +0700

    add product search service

---
+++ product-search-service/src/main/java/com/medievalgoose/product_search_service/ProductSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medievalgoose.product_search_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSearchServiceApplication.class, args);
+	}
+
+}
